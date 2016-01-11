579f73ec6f2038fa614399a88d78756bb0664905
Chris Gold <chrisgold2000@hotmail.com>
2016-01-11 18:04:35 +0000

    Add secure password and attribute to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :articles
   
-  before_save { self.email = emai.downcase }
+  before_save { self.email = email.downcase }
   
   validates :username, presence: true, 
             length: { minimum: 3, maximum: 25}, 
@@ -11,5 +11,5 @@ class User < ActiveRecord::Base
             length: {maximum: 105}, 
             uniqueness: {case_sensitive: false}, 
             format: { with: VALID_EMAIL_REGEX }
-  
+  has_secure_password
 end
