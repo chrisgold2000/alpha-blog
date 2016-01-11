1c861780e42a3ec79c1f2f1d8fdd87c794c089ce
Chris Gold <chrisgold2000@hotmail.com>
2016-01-11 17:07:07 +0000

    Add association between user and articles and add debugger view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :articles
+  
+  before_save { self.email = emai.downcase }
   
   validates :username, presence: true, 
             length: { minimum: 3, maximum: 25}, 
