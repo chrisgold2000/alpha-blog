bd643c78ca3bb1584ed813d11be4e8beba8263b7
Chris Gold <chrisgold2000@hotmail.com>
2016-01-13 13:13:45 +0000

    Fixed error in list_categories_test

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -5,10 +5,10 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
-      post_via_redirect categories_path, category: {name: "sports"}
+      post_via_redirect categories_path, category: {name: "Sports"}
     end
     assert_template 'categories/index'
-    assert_match "sports", response.body
+    assert_match "Sports", response.body
     
   end
   
